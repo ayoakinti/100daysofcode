5b4b5eecdf92a9f883c200a396122865085c4b0f
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-01-06 00:45:34 +0100

    .

--- Calculator/assets/js/main.js
+++ Calculator/assets/js/main.js
@@ -56,3 +56,26 @@ document.addEventListener("keydown", function (event) {
     }
   }
 });
+
+
+
+const rows = [
+  ["name1", "city1", "some other info"],
+  ["name2", "city2", "more info"]
+];
+
+let csvContent = "data:text/csv;charset=utf-8," 
+  + rows.map(e => e.join(",")).join("\n");
+
+//   var encodedUri = encodeURI(csvContent);
+// window.open(encodedUri);
+
+var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "my_data.csv");
+document.body.appendChild(link); // Required for FF
+
+// link.click();
+
+console.log(rows.map(e => e.join(",")).join("\n"));
